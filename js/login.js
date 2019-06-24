c8ecbe511a6759392de63474d4948adf52e29253
= <=>
2019-06-25 00:26:21 +0200

    Add the backend code for authentification

--- js/login.js
+++ js/login.js
@@ -12,7 +12,7 @@ var teacherPassword = "illbeback";
 btnValid.addEventListener("click",function login() {
 	var usernameValue=usernameInput.value;
 	var passwordValue=passwordInput.value;
-	if(usernameInput.value==pupilUsername && passwordInput.value==pupilPassword){
+	/*if(usernameInput.value==pupilUsername && passwordInput.value==pupilPassword){
 		console.log("Bonjour élève!");
 		window.location.href="../view/progress.html";
 	}
@@ -21,5 +21,16 @@ btnValid.addEventListener("click",function login() {
 		window.location.href="../view/teacherServiceChoice.html";
 	}
 	else console.log("Erreur d'authentification!");
+    */
 }
 );
+
+function genHash(text)
+{
+    var md = forge.md.sha256.create();
+    md.start();
+    md.update(text, "utf8");
+    var hashText = md.digest().toHex();
+
+    return hashText;
+}
