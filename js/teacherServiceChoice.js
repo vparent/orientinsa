b115ddeefbbfc233f879649bfe82af7bc6635c93
Quentin <quentin.couturier@insa-cvl.fr>
2019-06-27 12:03:12 +0200

    modifié :         css/addMarker.css 	nouveau fichier : css/race.css 	modifié :         css/teacherServiceChoice.css 	modifié :         js/addMarker.js 	modifié :         js/groupMaking.js 	modifié :         js/routeMaking.js 	modifié :         js/teacherServiceChoice.js 	modifié :         view/addMarker.html 	nouveau fichier : view/race.html 	modifié :         view/teacherServiceChoice.html

--- js/teacherServiceChoice.js
+++ js/teacherServiceChoice.js
@@ -3,6 +3,7 @@ document.getElementById("teacherName").textContent=teacherUsername;
 
 var addPupilBtn=document.getElementById("addPupil");
 var makeGroupBtn=document.getElementById("makeGroup");
+var addMarkerBtn=document.getElementById("addMarker");
 var makeRouteBtn=document.getElementById("makeRoute");
 var raceProgressBtn=document.getElementById("raceProgress");
 
@@ -11,6 +12,9 @@ addPupilBtn.addEventListener("click",function(){
 });
 makeGroupBtn.addEventListener("click",function(){
 	window.location.href="../view/groupMaking.html";
+});
+addMarkerBtn.addEventListener("click",function(){
+	window.location.href="../view/addMarker.html";
 });
 makeRouteBtn.addEventListener("click",function(){
 	window.location.href="../view/routeMaking.html";
